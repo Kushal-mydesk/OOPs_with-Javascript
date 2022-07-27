17ae70e89a2676b58bf9b795d2735dfb9e3bac4d
Kushal Mondal <kushal99mondal@gmail.com>
2022-07-27 12:14:34 +0530

    27july22

--- Promises.js
+++ Promises.js
@@ -12,9 +12,14 @@ function get_datas() {
     setTimeout(() => {
         let output = "";
         datas.forEach((data,index)=>{
-            output += `<li>${data.name}</li>`;
+            output += `
+            <ul>
+            <li>${data.name}</li>
+            <li>${data.age}</li>
+            </ul>`;
         })
-        document.body.innerHTML = output;
+        //document.body.innerHTML = output;
+        document.getElementById('overview').innerHTML = output;
     },1000);
 }
 
@@ -23,6 +28,7 @@ function create_data(new_data){
         setTimeout(() => {
             datas.push(new_data);
             let error  = false;
+            
             if(!error){
                 resolve();
             }
@@ -34,6 +40,16 @@ function create_data(new_data){
 }
 
 
+//If the promise returns 'resolve', then the promise is resolved, then it will take the 'then' function , otherwise if the promise returns 'reject'
+//then it will catch the error 
+create_data({name: "Vivek", age: 23}).then(get_datas).catch((error) =>{
+    console.log(error);
+});
 
-create_data({name: "Vivek", age: 23}).then(get_datas);
+async function start(){
+    await create_data({name: "Nigel", age: 23});
+    get_datas();
 
+}
+
+start();
